994c7f0020678b4935cd25d775b1699ebc677087
Gogoku7 <Gogoku7@users.noreply.github.com>
2019-05-17 23:23:37 +0200

    #8 Squad Strike default overlay created
    Created default overlay for Super Smash Bros. Ultimate - Squad Strike.
    
    Started refactoring work for Ultimate Singles & Ultimate Squad Strike on #5, but not finished work on.
    
    Also smoothed the transition between themes.

---
+++ LivestreamProductionManager/LivestreamProductionManager/FightingGames/SuperSmashBros/JavaScript/Snackbar.js
@@ -0,0 +1,11 @@
+﻿function showSnackbar(message) {
+    $("#snackbar").html(message);
+
+    var marginLeft = ($(window).width() - $("#snackbar").outerWidth()) / 2;
+    $("#snackbar").css("left", marginLeft >= 0 ? marginLeft : 0);
+
+    $("#snackbar").addClass("show");
+    setTimeout(function () {
+        $("#snackbar").removeClass("show");
+    }, 9000);
+}
